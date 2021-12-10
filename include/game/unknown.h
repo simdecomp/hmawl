4323c228327e6907150647500e60b77acd4249f0
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2021-12-10 10:11:25 -0700

    func_8002F39C

--- include/game/unknown.h
+++ include/game/unknown.h
@@ -9,3 +9,17 @@ u8* func_8002EC4C();
 float FLOAT_8034a14c;
 float FLOAT_8034a148;
 double func_8002EC24(int param);
+void func_8002F3E8(u32* param_1, u32 param_2);
+void func_8002F39C(u32 *param_1,u32 param_2);
+struct unknown_8002F39C
+{
+    u32 unk0;
+    u32 unk4;
+    u32 unk8;
+    u32 unkC;
+    u32 unk10;
+    u8 filler14[0x34-0x14];
+    u8 unk34;
+};
+
+void func_8002F3E8(struct unknown_8002F39C * param_1, u32 param_2);
---
+++ src/game/code_8002F39C.cpp
@@ -0,0 +1,11 @@
+#include "game/unknown.h"
+void func_8002F39C(struct unknown_8002F39C *param_1,u32 param_2)
+{
+  param_1->unk10 = param_2;
+  param_1->unk0 = 19;
+  param_1->unk4 = 15;
+  param_1->unkC = 0;
+  param_1->unk8 = 0xffffffff;
+  param_1->unk34 = 0;
+  func_8002F3E8(param_1,0);
+}
