cbd9bacc3407a5b48bc82c4a92225ca7537017b3
Christian <50080567+ChrisNonyminus@users.noreply.github.com>
2021-12-10 09:19:32 -0700

    rename a cpp file

--- include/game/unknown.h
+++ include/game/unknown.h
@@ -1,11 +1,11 @@
 #include "dolphin/types.h"
 u16 func_8002EC64();
-u32* lbl_803490E8;
-u32* func_8002EC5C();
-u32* lbl_803490E4;
-u32* func_8002EC54();
-u32* lbl_803490E0;
-u32* func_8002EC4C();
+u8* lbl_803490E8;
+u8* func_8002EC5C();
+u8* lbl_803490E4;
+u8* func_8002EC54();
+u8* lbl_803490E0;
+u8* func_8002EC4C();
 float FLOAT_8034a14c;
 float FLOAT_8034a148;
 double func_8002EC24(int param);
---
+++ src/game/CharDesignData.cpp
@@ -0,0 +1,11 @@
+#include "game/unknown.h"
+// TODO: functions func_8002BBB8, and more 
+u8* func_8002EC5C(void) {
+    return lbl_803490E8;
+}
+u8* func_8002EC54() {
+    return lbl_803490E4;
+}
+u8* func_8002EC4C() {
+    return lbl_803490E0;
+}
